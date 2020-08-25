9f690217db7ef675635dacb8813862328f5f3401
Shafi Masoumi <masoumi2017@icould.com>
2020-08-24 18:13:21 -0700

    Game is working now

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -13,7 +13,8 @@ const operations = [
   [-1, -1],
   [1, 0],
   [-1, 0]
-]
+];
+
 
 function App() {
   const [grid, setGrid] = useState(() => {
@@ -30,22 +31,33 @@ function App() {
   runningRef.current = running
   
   const runSimulation = useCallback(() => {
-    if (!running) {
+    if (!runningRef.current) {
       return;
     }
     setGrid((g) => {
       return produce(g, gridCopy => {
         for (let i = 0; i < numRows; i++) {
-          for (let k=0; k < numCols; k++) {
+          for (let k = 0; k < numCols; k++) {
             let neighbors = 0;
-            if(gridCopy[i-1][k-1] === 1) {
-              neighbors += 1;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK] 
+              }
+            })
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
             }
           }
         }
       });
     });
-    setTimeout(runSimulation, 1000)
+  
+    setTimeout(runSimulation, 200)
   }, []);
 
   return (
@@ -58,7 +70,11 @@ function App() {
         color: 'yellow'
       }}
       onClick={() => {
-        setRunning(!running)
+        setRunning(!running);
+        if (!running) {
+          runningRef.current = true;
+          runSimulation();
+        }
       }}
       >{running ? 'Stop' : 'Start'}</button>
       <div style={{
